b17c7c9610f9570cb7acc13275848be8c321ac8e
topazon <topazonzon@gmail.com>
2020-01-30 16:13:02 +0000

    Finish login-function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  # トップページ
   root 'static_pages#top'
+  # ログイン機能
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   
   resources :users
 end
